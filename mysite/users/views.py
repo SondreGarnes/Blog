bb8fec1b5cf95bb0f46546449d0f8b7474031867
Sondre Garnæs <sondre433@gmail.com>
2023-10-18 10:10:24 +0200

    Updates

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -4,16 +4,16 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
-    if request.method=="POST":
-        form=UserRegisterForm(request.POST)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
             form.save()
-            return redirect('blog-home')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
-        form=UserRegisterForm()
-    return render(request,'users/register.html',{'form':form})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
