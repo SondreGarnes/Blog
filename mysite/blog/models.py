80492b85c29a191ea0580a26230f9ab19cceb831
Sondre Garnæs <sondre433@gmail.com>
2023-10-18 14:17:06 +0200

    Optional to upload picture

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,17 +1,17 @@
+from typing import Any
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from PIL import Image
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
-    picture=models.ImageField(default="",upload_to='post_pics')
+    picture=models.ImageField(upload_to='post_pics',blank=True)
     created_at=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
 
-
+    
     def __str__(self):
         return self.title
     
