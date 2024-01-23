69867024f234270096bb1345afaab8253e8a3a4e
Sondre Garnæs <sondre433@gmail.com>
2024-01-23 15:35:31 +0100

    fixed it

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
